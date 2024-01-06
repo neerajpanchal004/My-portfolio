63aaa2df46396036f5a09b66a0128fd617c3634b
neerajpanchal004 <npvishwakarma.143@gmail.com>
2024-01-07 02:09:47 +0530

    c

---
+++ data/projectData.js
@@ -0,0 +1,51 @@
+export const projectData = [
+  {
+    name: "Whatsapp-clone",
+    alt: "A device showing screenshot of Whatsapp-clone",
+    img: "/img/whatsapp-clone.png",
+    description:"This is a clone of WhatsApp. In this project, I created a static navbar and three different pages like chats, updates, and calls. That is similar to the real WhatsApp",
+    techStack: "Next.js, Tailwind CSS",
+    live: "https://whats-app-five-sable.vercel.app/",
+    code: "https://github.com/neerajpanchal004/WhatsApp",
+  },
+  {
+    name: "Todo-app",
+    alt: "A device showing screenshot of todo-app",
+    img: "/img/todo-app.png",
+    description:"This is todo-app that can very usefull in our daily life it is used to remember and complete our daily tasks.",
+    techStack: "Next.js,Tailwind CSS",
+    live: "https://to-do-app-five-liart.vercel.app/",
+    code: "https://github.com/neerajpanchal004/To-do-app",
+  },
+  {
+    name: "Weather App",
+    alt: "A device showing screenshot of weather app",
+    img: "/img/weather-app.png",
+    description:
+      "This app shows the current weather of a city. It uses OpenWeatherMap API to fetch the weather data. It can also show weather based on the user's current location using Geolocation API. The app changes the background according to different weather conditions.",
+    techStack:
+      "JavaScript, HTML, CSS, Webpack, Jest, OpenWeatherMap API, Geolocation API",
+    live: "https://sneharatnani.github.io/weather-app/",
+    code: "https://github.com/sneharatnani/weather-app",
+  },
+  {
+    name: "Memory Game",
+    alt: "A device showing screenshot of memory game",
+    img: "/img/memory-game.png",
+    description:
+      "As the name suggests, it is a game to test players' memory. The emojis shuffle after every selection. The goal is to maximize your score by not selecting the same emoji again. The game shows the current and best score of a player. It is Unit Tested with Jest and React Testing Library.",
+    techStack: "React.js, Tailwind CSS, Jest, Testing Library",
+    live: "https://sneharatnani.github.io/memory-game/",
+    code: "https://github.com/sneharatnani/memory-game",
+  },
+  {
+    name: "Calculator",
+    alt: "A device showing screenshot of calculator",
+    img: "/img/calculator.png",
+    description:
+      "It is a simple Javascript calculator which can handle your daily calculations easily. It also has keyboard support.",
+    techStack: "JavaScript, HTML, CSS",
+    live: "https://sneharatnani.github.io/calculator/",
+    code: "https://github.com/sneharatnani/calculator",
+  },
+];
---
+++ data/skillsData.js
@@ -0,0 +1,46 @@
+export const skillsData = [
+  {
+    image: "/icons/html5.svg",
+    caption: "HTML",
+  },
+  {
+    image: "/icons/css.svg",
+    caption: "CSS",
+  },
+  {
+    image: "/icons/javascript.svg",
+    caption: "JavaScript",
+  },
+  {
+    image: "/icons/react.svg",
+    caption: "React.js",
+  },
+ 
+  {
+    image: "/icons/tailwindcss.svg",
+    caption: "Tailwind CSS",
+  },
+  
+  {
+    image: "/icons/nextjs.svg",
+    caption: "Next.js",
+  },
+  {
+    image: "/icons/github-logo.png",
+    caption: "Github",
+  },
+
+ 
+  {
+    image: "/icons/git.svg",
+    caption: "Git",
+  },
+  {
+    image: "/icons/postman.svg",
+    caption: "Postman",
+  },
+  {
+    image: "/icons/Vercel.svg",
+    caption: "Vercel",
+  },
+];
