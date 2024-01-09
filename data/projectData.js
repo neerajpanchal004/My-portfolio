84d83b13f28ff74c345fae7da16bf91947ee7897
neerrajpanchal004 <npvishwakarma.143@gmail.com>
2024-01-09 13:25:45 +0530

    fix

--- data/projectData.js
+++ data/projectData.js
@@ -17,35 +17,15 @@ export const projectData = [
     live: "https://to-do-app-five-liart.vercel.app/",
     code: "https://github.com/neerajpanchal004/To-do-app",
   },
+  
   {
-    name: "Weather App",
-    alt: "A device showing screenshot of weather app",
-    img: "/img/weather-app.png",
-    description:
-      "This app shows the current weather of a city. It uses OpenWeatherMap API to fetch the weather data. It can also show weather based on the user's current location using Geolocation API. The app changes the background according to different weather conditions.",
-    techStack:
-      "JavaScript, HTML, CSS, Webpack, Jest, OpenWeatherMap API, Geolocation API",
-    live: "https://sneharatnani.github.io/weather-app/",
-    code: "https://github.com/sneharatnani/weather-app",
-  },
-  {
-    name: "Memory Game",
-    alt: "A device showing screenshot of memory game",
+    name: "Advice-generator-app",
+    alt: "A device showing screenshot of Advice-app",
     img: "/img/memory-game.png",
-    description:
-      "As the name suggests, it is a game to test players' memory. The emojis shuffle after every selection. The goal is to maximize your score by not selecting the same emoji again. The game shows the current and best score of a player. It is Unit Tested with Jest and React Testing Library.",
-    techStack: "React.js, Tailwind CSS, Jest, Testing Library",
-    live: "https://sneharatnani.github.io/memory-game/",
-    code: "https://github.com/sneharatnani/memory-game",
+    description:"As the name suggests, it is an advice app if you click on the dice button every time it returns beautiful advice.",
+    techStack: "API,JavaScript, HTML, CSS",
+    live: "https://advice-generator-app-seven-gamma.vercel.app/",
+    code: "https://github.com/neerajpanchal004/Advice-generator-app",
   },
-  {
-    name: "Calculator",
-    alt: "A device showing screenshot of calculator",
-    img: "/img/calculator.png",
-    description:
-      "It is a simple Javascript calculator which can handle your daily calculations easily. It also has keyboard support.",
-    techStack: "JavaScript, HTML, CSS",
-    live: "https://sneharatnani.github.io/calculator/",
-    code: "https://github.com/sneharatnani/calculator",
-  },
+ 
 ];
